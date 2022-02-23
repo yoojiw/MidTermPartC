1071c9dd7e1bf0476adb367d0338170db18e7f55
yoojiw <yoojiw@sheridancollege.ca>
2022-02-23 10:06:23 -0500

    added new class PartTimeStudentvv

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+import java.util.Random;
+
+/**
+ *
+ * @author irene
+ */
+public class TestUserprofile {
+ 
+    public static void makeProfile(){
+        UserProfile profile = new UserProfile("ID12345", "Comedy"); 
+        
+        System.out.println("userProfile was created");
+       
+    }
+}
